d16671c1fc9402fcb7d8b9d59b61efaa899c0869
Muchnik Illia <muchnik.illia.js@gmail.com>
2022-12-22 10:34:12 +0200

    Fix warning

--- src/components/CountDown/CountDown.js
+++ src/components/CountDown/CountDown.js
@@ -19,7 +19,7 @@ export const CountDown = ({ hours = 0, minutes = 0, seconds = 0 , startPauseMusi
       setOver(true)
     } else if (m === 0 && s === 0) {
       setTime([h - 1, 59, 59])
-    } else if (s == 0) {
+    } else if (s === 0) {
       setTime([h, m - 1, 59])
     } else {
       setTime([h, m, s - 1])
